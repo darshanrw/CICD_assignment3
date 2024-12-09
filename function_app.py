7d649afa366be6829de60d94b371b27e03d282e4
darshanrw <Dwadekar4575@conestogac.on.ca>
2024-12-08 23:12:54 -0500

    pushing function files

---
+++ function_app.py
@@ -0,0 +1,13 @@
+import azure.functions as func
+import logging
+ 
+app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+ 
+@app.route(route="http_trigger1")
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+ 
+    return func.HttpResponse(
+        "Hello, Prof Aagam!, Assignment3 Completed 8934575!",
+        status_code=200
+    )
